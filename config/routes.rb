38a4329c3fcb55325a42fb2db16ecd8d5a681c13
Guillaume Corlay <guillaume.corlay-van-der-schaal@hec.edu>
2016-08-09 15:51:18 +0200

    done with 02 for real

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :tasks
+  # #list all tasks
+  # get "tasks" => "tasks#index"
+
+  # #list one task
+  # get "task/:id" => "tasks#show"
+
+  # #create a new task
+  # get "tasks/new" => "tasks#new"
+  # post "tasks" => "tasks#create"
+
+  # #update a new task
+  # get "tasks/:id/edit" => "tasks#edit"
+  # patch "tasks/:id" => "tasks#update"
+
+  # #destroy a task
+  # delete "tasks/:id" => "tasks#delete"
 end
